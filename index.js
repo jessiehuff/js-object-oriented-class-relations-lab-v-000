8c89937edfb82092cc8ec1cf46e189198293c3ad
Jessie Huff <huffjd1@gmail.com>
2018-03-08 20:33:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,12 +53,12 @@ class Trip{
     this.passengerId = passenger.id
   }
   driver(){
-    return store.drivers.find(function(driver){
+    return store.drivers.find(driver => {
       return driver.id === this.driverId
     })
   }
   passenger(){
-    return store.passengers.find(function(passenger){
+    return store.passengers.find(passenger => {
       return passenger.id === this.passengerId
     })
   }
